175953ca3ac4a687e0981395ae739f9fb9fc23fb
TomoyukiNakamura <nakamura_t@ss.osaka-kyoiku.ac.jp>
2017-01-11 15:20:17 +0900

    Add files via upload

---
+++ lang/en/office.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for office
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_office
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Office On Moodle';
+$string['modulenameplural'] = 'offices';
+$string['modulename_help'] = 'The Office On Moodle module enable to use Office Online in Moodle.
+
+You can select "share file" or "view file" from your Onedrive.
+And can select group for "share file". 
+
+Office On Moodle module has many uses, such as
+
+・group work for collaboration learning
+
+・edit "share file" with many user
+
+・distribution "view file"
+';
+$string['office:addinstance'] = 'Add a new office';
+$string['office:submit'] = 'Submit office';
+$string['office:view'] = 'View office';
+$string['officefieldset'] = 'Custom example fieldset';
+$string['officename'] = 'Title';
+$string['officename_help'] = 'Title for viewpage of Office On Moodle.';
+$string['office'] = 'office';
+$string['onedrive_share'] = 'Select for "share file"';
+$string['onedrive_share_setumei'] = 'Click "Open to OneDrive for ShareLink" button , and select "share file"';
+$string['onedrive_share_filename'] = 'Share File Name';
+$string['onedrive_share_group'] = 'Share Group Name';
+$string['onedrive_view'] = 'Select for "View file"';
+$string['onedrive_view_setumei'] = 'Click "Open to OneDrive for ViewLink" button , and select "view file"';
+$string['onedrive_view_filename'] = 'View File Name';
+$string['pluginadministration'] = 'office administration';
+$string['pluginname'] = 'office';
+$string['select_group'] = 'Select Group';
+$string['setting_head'] = 'Setting "Application Registration"';
+$string['setting_setumei'] = 'Refer to "How to register of app" and enter clientID';
+$string['setting_lang'] = 'us';
+$string['view_head1'] = 'Share Link';
+$string['view_head2'] = '(If you want edit , select to "Edit in Browser" in "Edit Document")';
+$string['view_head3'] = 'View Link';
---
+++ lang/ja/office.php
@@ -0,0 +1,71 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for office
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_office
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Office On Moodle';
+$string['modulenameplural'] = 'offices';
+$string['modulename_help'] = 'Office On Moodleを利用することで、あなたのOnedriveに保存されているデータを共有リンクとして表示することが可能になります。
+
+共有リンクは、共同編集可能リンクと閲覧可能リンクの2種類作ることが可能です。
+初期設定のページで、あなたのOnedriveにアクセスし、共有したいファイルを選択することで選択されたファイルの共有リンクが表示される様になります。
+また、Moodleのグループ機能と連携することでグループごとに共同編集可能リンクを配布することが可能です。
+
+ただし、学生ユーザは少くとも1つグループに属していないと共同編集リンクは表示されないのでご注意下さい。
+<br><br>
+
+
+Office On Moodleは次のことを容易に行うことが可能になります。
+
+・学生同士でグループ学習型協働学習を行う。<br>
+・学生にデータを配布、表示する。<br>
+・複数人で文書を編集するためのリンク表示。<br>
+';
+$string['office:addinstance'] = 'Add a new office';
+$string['office:submit'] = 'Submit office';
+$string['office:view'] = 'View office';
+$string['officefieldset'] = 'Custom example fieldset';
+$string['officename'] = 'ページのタイトル';
+$string['officename_help'] = 'Office onlineの共有リンクを表示するページのタイトルです。';
+$string['office'] = 'office';
+$string['onedrive_share'] = '編集可能なファイルの選択';
+$string['onedrive_share_setumei'] = '右の「Open to Onedrive for ShareLink」をクリックし、共有したいファイルを選択してください。';
+$string['onedrive_share_filename'] = '共有するファイル名';
+$string['onedrive_share_group'] = '共有するグループ';
+$string['onedrive_view'] = '閲覧可能な共有ファイル';
+$string['onedrive_view_setumei'] = '右の「Open to Onedrive for ViewLink」をクリックし、共有したいファイルを選択してください。';
+$string['onedrive_view_filename'] = '共有するファイル名';
+$string['pluginadministration'] = 'office administration';
+$string['pluginname'] = 'office';
+$string['select_group'] = 'グループを選択して下さい';
+$string['setting_head'] = 'App登録設定';
+$string['setting_setumei'] = '以下の「アプリケーション登録の方法」を参考にしてアプリケーション登録を行い、clientIDを入力して下さい';
+$string['setting_lang'] = 'ja';
+$string['view_head1'] = '共有リンク';
+$string['view_head2'] = '(編集を行う場合は「ブラウザーで編集」をクリックして下さい)';
+$string['view_head3'] = '閲覧リンク';
